100e2a03cec7c7171bfea128016af50d0ac4f2bd
raz3rbla8e <email@example.com>
2023-12-08 16:17:11 -0500

    saveed this shut

--- mainRouter.js
+++ mainRouter.js
@@ -26,4 +26,36 @@ router.get("/home", async function (req, res) {
 
 });
 
+
+router.get("/art/:id", async function (req, res) {
+
+    if (req.session.user) {
+        try {
+            const artwork = await Artwork.findById(req.params.id);
+
+            if (!artwork) {
+                res.status(404).send('Not Found');
+                return;
+            }
+            let username = artwork.Artist;
+
+            let user = await User.findOne({ username: username });
+
+            if (user) {
+                let userid = user._id;
+                console.log(userid);
+                res.render("art", { artwork: artwork, session: req.session, userid: userid });
+            } else {
+                res.status(404).send('User not found');
+            }
+        } catch (error) {
+            console.error('Error fetching artwork:', error);
+            res.status(500).send('Internal Server Error');
+        }
+    }
+    else {
+        res.redirect("/account/login");
+    }
+});
+
 module.exports = router;
